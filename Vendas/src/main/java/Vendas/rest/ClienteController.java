9914ccb605875b15bbaad5d4dcd1f1e3a0f86610
marcus aurelio <marcusaurelio743@gmail.com>
2025-03-04 18:17:51 -0300

    criando metodo de deletar cliente

--- Vendas/src/main/java/Vendas/rest/ClienteController.java
+++ Vendas/src/main/java/Vendas/rest/ClienteController.java
@@ -2,11 +2,15 @@ package Vendas.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import Vendas.model.entity.Cliente;
 import Vendas.model.repository.ClienteRepository;
@@ -23,6 +27,25 @@ public class ClienteController {
 	public Cliente salvar(@RequestBody Cliente cliente){
 		return repository.save(cliente);
 		
+	}
+	
+	@GetMapping("{id}")
+	public Cliente buscarPorId(@PathVariable Long id) {
+		return repository
+				.findById(id)
+					.orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+	
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Long id) {
+		 repository
+				.findById(id)
+				.map(cliente->{
+					repository.delete(cliente);
+					return Void.TYPE;
+				})
+					.orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 
 }
